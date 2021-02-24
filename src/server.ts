7668373a8fa00a6f83583d3878bb5292deeab1c0
ArturBatista <35811473+ArturBatista@users.noreply.github.com>
2021-02-24 00:34:24 -0300

    Aula 02 - Banco de Dados
    Configurado o TypeORM, instalado o SQLite, e criado o controller, model e migration de User

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
-
+import "./database";
+import { router } from './routes';
 const app = express();
 
-app.get("/",(request, response)=>{
+app.use(express.json());
+app.use(router);
 
-    return response.json({message: "Hello Worlds - NLW04"} );
-    //return response.send("Hello World - NWL#4");
-});
-
-//1 para => Rota(recurso Api)
-//2 param => request, response
-
-app.post("/users", (request, response)=>{
-
-    //Recebeu os dados para salvar
-    return response.json({
-        message: "Os dados foram salvos com sucesso!"
-    });
-});
 
 app.listen(3333, ()=> console.log("Server is running!"));
